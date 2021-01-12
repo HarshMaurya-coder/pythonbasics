472cc12faf8fb2f54e1e026d8df8d00dbdac265e
harshcoder <ramkumarpagal1@gmail.com>
2021-01-12 15:43:07 +0000

    num = int(input("Enter first number")) num2 = int(input("Enter second number"))
    print("press 1 for addition")
    operator = input()
    
    if operator == "1":
       	 print(num + num2)

---
+++ session1/addition.py
@@ -0,0 +1,8 @@
+  num1 = int(input("Enter first number: "))
+  num2 = int(input("Enter second number: "))
+
+  print("press 1 for addition")
+  operator = input()
+
+  if operator == "1":
+          print(num1 + num2)
--- session1/hello.py
+++ session1/hello.py
@@ -1,1 +1,9 @@
- 1 print("hello world")
+1 num = int(input("Enter first number"))
+2 num2 = int(input("Enter second number"))
+3
+4 print("press 1 for percentage")
+5 operator = input()
+6 
+7 if operator == "1"
+8     print(num / num2 * 100)
+
